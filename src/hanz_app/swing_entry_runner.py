8f42a93df06b4e0b28dfcbddee356bfc5254b830
fazabihandoko28 <fazabihandoko@gmail.com>
2026-09-14 07:24:50 +0400

    feat: install HANZ support-bounce intelligence

--- src/hanz_app/swing_entry_runner.py
+++ src/hanz_app/swing_entry_runner.py
@@ -4,14 +4,16 @@ from . import swing_trading_engine as engine
 from .momentum_entry_score import momentum_entry_score
 from .early_momentum_trigger import install as install_early_momentum_trigger
 from .market_data_router import install as install_market_data_router
+from .support_bounce_intelligence import install as install_support_bounce_intelligence
 
 
 # Keep CRV3 prefix for dashboard backwards compatibility.
-SCORE_VERSION = "CRV3_MES5_2026_09_11_STRICT_TIMING"
+SCORE_VERSION = "CRV3_MES5_BOUNCE_2026_09_14_INTRADAY"
 
 
 def main():
     install_market_data_router(engine)
+    install_support_bounce_intelligence(engine)
     engine.canonical_rank_score = momentum_entry_score
     engine.CANONICAL_RANK_VERSION = SCORE_VERSION
     install_early_momentum_trigger(engine)
